448b73ef760a8450ff2fa2d8bdeaef66c6e975b1
babacar ndiaye <babacar.ndiaye20@uvs.edu.sn>
2023-03-16 02:29:16 +0000

    final commit

--- src/main/java/Jiraws_live_youtube/Contact.java
+++ src/main/java/Jiraws_live_youtube/Contact.java
@@ -1,5 +1,41 @@
 package Jiraws_live_youtube;
 
 public class Contact {
+	
+	private String lastName;
+	private String fistName;
+	private String phoneNumber;
+	
+	
+	public Contact (String lastName, String fistName, String phoneNumber) {
+		this.lastName= lastName;
+		this.fistName = fistName;
+		this.phoneNumber = phoneNumber;
+	}
+ 
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFistName() {
+		return fistName; 
+	}
+	public void setFistName(String fistName) {
+		this.fistName = fistName;
+	}
+	public String  getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) { 
+		this.phoneNumber = phoneNumber;
+	}
+	
+	@Override
+	public String toString() {
+		return this.lastName + " " + this.fistName + " " + this.phoneNumber ;
+	}
+	
 
 }
--- src/main/java/Jiraws_live_youtube/PhoneBook.java
+++ src/main/java/Jiraws_live_youtube/PhoneBook.java
@@ -1,10 +1,15 @@
 package Jiraws_live_youtube;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class PhoneBook {
 	
-    public static Scanner sc = null;
+    public static Scanner sc = null; 
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,11 +20,38 @@ public class PhoneBook {
 	    String userFirstName = getUserInput("Entrez un prénom ");
 	    String userPhoneNumber = getUserInput("Entrez un numero de telephone ");
 		
-	    System.out.println(userLastName);
-	    System.out.println(userFirstName);
-	    System.out.println(userPhoneNumber);
+	    Contact newContact = new Contact(userLastName, userFirstName, userPhoneNumber);
+	    
+	    System.out.println(newContact.toString() );
+	    
+	    File phoneBookFile = new File("C:\\Users\\Lenovo-T440\\Desktop\\Projet testeur logicielle\\Contact.txt");
+	    
+	    if(phoneBookFile.exists()&&phoneBookFile == null) {
+	    	System.out.println("le fichier existe a l'emplacement donné");
+	    }
+	    else {
+	    	System.out.println("le fichier n'existe pas");
+	    	try {
+				phoneBookFile.createNewFile();
+				System.out.println("le fichier a été creer");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+	    
+	    try {
+			BufferedWriter fileWriter = new BufferedWriter(new FileWriter(phoneBookFile, true));
+		    fileWriter.append(newContact.toString()+ '\n');
+		    fileWriter.close();
+	    } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    
 		sc.close();
-
+ 
 	}
 	
 	public static String getUserInput(String userRequest) {
@@ -30,5 +62,7 @@ public class PhoneBook {
 		return userLastName;
 		
 	}
+	
+	
 
 }
